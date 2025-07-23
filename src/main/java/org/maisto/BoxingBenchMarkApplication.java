c08bcc41d03a58b4a6ccdeed95e8bbd7592f4769
genny51 <raffaelemaisto1987@gmail.com>
2025-07-23 19:45:05 +0200

    adds comments on parallel stream part

--- src/main/java/org/maisto/BoxingBenchMarkApplication.java
+++ src/main/java/org/maisto/BoxingBenchMarkApplication.java
@@ -2,16 +2,16 @@ package org.maisto;
 
 import org.openjdk.jmh.annotations.*;
 
-@BenchmarkMode(Mode.AverageTime) //It measures the average time of benchmarked methods
-@OutputTimeUnit(java.util.concurrent.TimeUnit.MILLISECONDS)
-@Fork(value = 2,jvmArgs = {"-Xms4G","-Xmx4G"})//It exetus the benchmarck 2 times to improve the reliability of results and uses 4Gb of heap space to limit the influence of garbage collector
+//@BenchmarkMode(Mode.AverageTime) //It measures the average time of benchmarked methods
+//@OutputTimeUnit(java.util.concurrent.TimeUnit.MILLISECONDS)
+//@Fork(value = 2,jvmArgs = {"-Xms4G","-Xmx4G"})//It exetus the benchmarck 2 times to improve the reliability of results and uses 4Gb of heap space to limit the influence of garbage collector
 public class BoxingBenchMarkApplication {
 
     private static final int N = 1000_000 ;
 
     /*We want to measure the consequences of using Integer in place of int*/
 
-    @Benchmark
+  //  @Benchmark
     public int intSum(){
         int sum = 0;
         for(int i = 0; i <= N; i++){
@@ -20,7 +20,7 @@ public class BoxingBenchMarkApplication {
         return sum;
     }
 
-    @Benchmark
+  //  @Benchmark
     public Integer integerSum(){
         Integer sum = 0 ;
         for(int i = 0; i <= N; i++){
@@ -28,7 +28,5 @@ public class BoxingBenchMarkApplication {
         }
         return sum;
     }
-
-
 
 }
